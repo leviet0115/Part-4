2ef7c50f2ebeb4d14de19de4997540daf375d015
Le Viet <le.viet@MacBook-Pro-2.local>
2022-05-03 14:13:48 +0300

    exercise 4.1 - 4.2

---
+++ index.js
@@ -0,0 +1,10 @@
+const app = require("./app");
+const http = require("http");
+const { PORT } = require("./utils/config");
+
+const server = http.createServer(app);
+
+//need to set env var
+server.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
